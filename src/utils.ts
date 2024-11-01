70f51dbce9382dba1882a3b713bd3a6f4b2593d5
Andrew Fraser <andrew@dkoda.xyz>
2024-11-01 16:58:06 +0100

    getMultipleTokens, getNativeBalance, getMaxWithdraw

--- src/utils.ts
+++ src/utils.ts
@@ -1,7 +1,15 @@
-import { PublicKey, Connection } from "@solana/web3.js";
+import { PublicKey, Connection, LAMPORTS_PER_SOL } from "@solana/web3.js";
 import { Program, utils, BN } from "@coral-xyz/anchor";
 import { WasabiSolana } from "../idl/wasabi_solana";
-import { TOKEN_PROGRAM_ID, TOKEN_2022_PROGRAM_ID, MintLayout } from "@solana/spl-token";
+import {
+    TOKEN_PROGRAM_ID,
+    TOKEN_2022_PROGRAM_ID,
+    AccountLayout,
+    MintLayout,
+    getTokenMetadata,
+    getAssociatedTokenAddressSync,
+} from "@solana/spl-token";
+import { Metaplex } from '@metaplex-foundation/js';
 
 export const WASABI_PROGRAM_ID = new PublicKey("Amxm1TKpMsue3x5KrnAzV9U8Sn7afDQQnmMV9znTfd96");
 
@@ -158,5 +166,108 @@ export const PDA = {
             [utils.bytes.utf8.encode(SEED_PREFIX.GLOBAL_SETTINGS)],
             WASABI_PROGRAM_ID,
         );
+    }
+}
+
+type TokenData = {
+    decimals: number;
+    name?: string;
+    symbol?: string;
+    address: string;
+}
+
+export async function getMintInfo(connection: Connection, mint: PublicKey): Promise<TokenData | null> {
+    try {
+        let metadata;
+
+        const mintAccount = await connection.getAccountInfo(mint);
+        if (!mintAccount) return null;
+
+        if (mintAccount.owner.equals(TOKEN_2022_PROGRAM_ID)) {
+            metadata = await getTokenMetadata(
+                connection,
+                mint,
+                'confirmed'
+            );
+
+            if (!metadata) {
+                metadata = await getMetaplexMetadata(connection, mint);
+            }
+        } else {
+            metadata = await getMetaplexMetadata(connection, mint);
+        }
+
+        const mintInfo = MintLayout.decode(mintAccount.data);
+
+        if (!metadata) return null;
+
+        return {
+            decimals: mintInfo.decimals,
+            name: metadata.name,
+            symbol: metadata.symbol,
+            address: mint.toString()
+        };
+    } catch (error) {
+        return null;
+    }
+}
+
+export async function getMetaplexMetadata(connection: Connection, mint: PublicKey): Promise<{
+    name: string,
+    symbol: string,
+} | null> {
+    try {
+        const metaplex = Metaplex.make(connection);
+        const metadata = await metaplex.nfts().findByMint({ mintAddress: mint });
+        return {
+            name: metadata.name,
+            symbol: metadata.symbol,
+        };
+    } catch (error) {
+        return null;
+    }
+}
+
+// Mint is the mint of the token in the vault
+export async function getMaxWithdraw(program: Program<WasabiSolana>, mint: PublicKey): Promise<number | null> {
+    const lpVaultAddress = PDA.getLpVault(mint);
+    const sharesMintAddress = PDA.getSharesMint(lpVaultAddress, mint);
+    const userSharesAddress = getAssociatedTokenAddressSync(
+        sharesMintAddress,
+        program.provider.publicKey,
+        false,
+        TOKEN_2022_PROGRAM_ID
+    );
+    const [lpVault, userSharesInfo, sharesMintInfo] = await Promise.all([
+        program.account.lpVault.fetch(lpVaultAddress),
+        program.provider.connection.getAccountInfo(userSharesAddress),
+        program.provider.connection.getAccountInfo(sharesMintAddress),
+    ]);
+
+    if (!lpVault || !userSharesInfo || !sharesMintInfo) return null;
+
+
+    const sharesMint = MintLayout.decode(sharesMintInfo.data);
+    const userShares = AccountLayout.decode(userSharesInfo.data);
+
+    const mintDecimals = sharesMint.decimals;
+    const totalAssets = new BN(lpVault.totalAssets);
+    const totalShares = new BN(sharesMint.supply);
+    const userSharesAmount = new BN(userShares.amount);
+
+    if (totalShares.isZero()) return null;
+
+    const maxWithdrawRaw = userSharesAmount.mul(totalAssets).div(totalShares);
+
+    return Number(maxWithdrawRaw.toString()) / Math.pow(10, mintDecimals);
+}
+
+export async function getNativeBalance(connection: Connection, userAddress: PublicKey): Promise<number | null> {
+    try {
+        const balance = await connection.getBalance(userAddress);
+        return balance / LAMPORTS_PER_SOL
+    } catch (error) {
+        console.error("Error fetch SOL balance:", error);
+        return null;
     }
 }
