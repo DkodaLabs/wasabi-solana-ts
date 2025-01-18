e414a32decfd7147f5e634f13b77d31a4d396b3c
hasanatay <hasan@dkoda.xyz>
2025-01-18 14:28:49 -0500

    index

--- src/index.ts
+++ src/index.ts
@@ -2,5 +2,7 @@ export * from './utils/index.js';
 export * from './base/index.js';
 export * from './instructions/index.js';
 export * from './idl/index.js';
+export * from './error-handling/index.js';
+export * from './compute-budget/index.js';
 import * as idl from './idl/wasabi_solana.json';
 export { idl };
