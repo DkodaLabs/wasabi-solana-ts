dbc25e080ef284b87e915d620d6dd59b6829640c
hasanatay <hasan@dkoda.xyz>
2025-01-18 14:27:11 -0500

    error handling + compute budget

---
+++ src/compute-budget/computeBudget.ts
@@ -0,0 +1,86 @@
+import {
+    ComputeBudgetProgram,
+    Connection,
+    PublicKey,
+    TransactionInstruction
+} from '@solana/web3.js';
+
+export type ComputeBudgetConfig = {
+    // Fee type, default is DYNAMIC
+    type?: 'DYNAMIC' | 'FIXED';
+
+    // Fee speed (only used if type is DYNAMIC), default is NORMAL
+    speed?: 'NORMAL' | 'FAST' | 'TURBO';
+
+    // Used as max priority fee if type is DYNAMIC, or as fixed fee if type is FIXED
+    price: number;
+
+    // Compute unit limit
+    limit?: number;
+};
+
+const DEFAULT_COMPUTE_LIMIT = 400_000;
+const DEFAULT_UNIT_PRICE = 50_000;
+
+const SPEED_BUFFERS = {
+    NORMAL: 1.1,
+    FAST: 2,
+    TURBO: 4
+} as const;
+
+async function getDynamicPriorityFee(
+    connection: Connection,
+    writableAccounts: PublicKey[],
+    speed: 'NORMAL' | 'FAST' | 'TURBO'
+): Promise<number> {
+    let recentFees = await connection.getRecentPrioritizationFees({
+        lockedWritableAccounts: writableAccounts
+    });
+    recentFees = recentFees.filter(r => r.prioritizationFee > 0);
+
+    if (!recentFees.length) {
+        return DEFAULT_UNIT_PRICE * SPEED_BUFFERS[speed];
+    }
+
+    const sortedFees = recentFees
+        .map(fee => fee.prioritizationFee)
+        .sort((a, b) => a - b);
+
+    const midPoint = Math.floor(sortedFees.length / 2);
+    const medianFee = sortedFees.length % 2 === 0
+        ? (sortedFees[midPoint - 1] + sortedFees[midPoint]) / 2
+        : sortedFees[midPoint];
+
+    return Math.ceil(medianFee * SPEED_BUFFERS[speed]);
+}
+
+function getWritableAccounts(instructions: TransactionInstruction[]): PublicKey[] {
+    return instructions
+        .flatMap(ix => ix.keys)
+        .filter(meta => meta.isWritable)
+        .map(meta => meta.pubkey)
+        .filter((v, i, a) => a.findIndex(t => t.equals(v)) === i);
+}
+
+export async function createComputeBudgetIx(
+    connection: Connection,
+    request: ComputeBudgetConfig,
+    instructions: TransactionInstruction[]
+): Promise<TransactionInstruction[]> {
+    if (request.type === 'FIXED') {
+        return [
+            ComputeBudgetProgram.setComputeUnitLimit({ units: request.limit || DEFAULT_COMPUTE_LIMIT }),
+            ComputeBudgetProgram.setComputeUnitPrice({ microLamports: request.price })
+        ];
+    }
+
+    const writableAccounts = getWritableAccounts(instructions);
+
+    let price = await getDynamicPriorityFee(connection, writableAccounts, request.speed || "NORMAL");
+    price = Math.min(price, request.price);
+
+    return [
+        ComputeBudgetProgram.setComputeUnitLimit({ units: request.limit || DEFAULT_COMPUTE_LIMIT }),
+        ComputeBudgetProgram.setComputeUnitPrice({ microLamports: price })
+    ];
+}
---
+++ src/error-handling/transactionErrorHandling.ts
@@ -0,0 +1,75 @@
+
+import { IDL as JupiterIDL } from './jupiter';
+import { SendTransactionError, VersionedTransaction } from '@solana/web3.js';
+import {idl} from "../index";
+
+type ErrorObject = {
+    code: number;
+    name: string;
+    msg: string;
+    expected: boolean;
+    program: string;
+};
+
+const wasabiProgramId = "spicyTHtbmarmUxwFSHYpA8G4uP2nRNq38RReMpoZ9c";
+const wasabiExpectedErrors = [
+    6026, // LiquidationThresholdNotReached
+    6017, // PriceTargetNotReached
+];
+
+const jupiterProgramId = "JUP6LkbZbjS1jKKwapdHNy74zcZ3tLUZoi5QNyVTaV4";
+const jupiterExpectedErrors = [
+    6001, // SlippageToleranceExceeded
+];
+
+// Index errors by code for quick lookup
+const wasabiErrorIndex: Record<number, ErrorObject> = idl.errors.reduce((acc: Record<number, ErrorObject>, error) => {
+    const expected = wasabiExpectedErrors.includes(error.code);
+    acc[error.code] = {
+        ...error,
+        expected,
+        program: 'Wasabi'
+    }
+    return acc;
+}, {});
+
+const findWasabiError = (code: number): ErrorObject | undefined => {
+    return wasabiErrorIndex[code];
+}
+
+const jupiterErrorIndex: Record<number, ErrorObject> = JupiterIDL.errors.reduce((acc: Record<number, ErrorObject>, error) => {
+    const expected = jupiterExpectedErrors.includes(error.code);
+    acc[error.code] = {
+        ...error,
+        expected,
+        program: 'Jupiter'
+    };
+    return acc;
+}, {});
+
+const findJupiterError = (code: number): ErrorObject | undefined => {
+    return jupiterErrorIndex[code];
+}
+
+export const parseSendTransactionError = (error: SendTransactionError, transaction: VersionedTransaction): ErrorObject | undefined => {
+    const message = error.transactionError.message;
+
+    // Parse error messages like: "Transaction simulation failed: Error processing Instruction 3: custom program error: 0x1771"
+    const match = message.match(/Instruction (\d+):.*(0x[0-9a-fA-F]+)/);
+
+    if (match) {
+        const instructionNumber = parseInt(match[1]); // First capture group
+        const errorCode = parseInt(match[2], 16); // Second capture group, comes as hex
+
+        // Validate instruction against parsed addresses
+        const instruction = transaction.message.compiledInstructions[instructionNumber];
+        const programId = transaction.message.staticAccountKeys[instruction.programIdIndex].toBase58();
+
+        if (programId === wasabiProgramId) {
+            return findWasabiError(errorCode);
+        } else if (programId === jupiterProgramId) {
+            return findJupiterError(errorCode);
+        }
+    }
+    return undefined;
+}
