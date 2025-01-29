77d5fcc5fa91535fd7b6efaf15edaa4d00ba3a26
Andrew Fraser <serfrae@pm.me>
2025-01-28 21:10:35 -0500

    sim error handling

--- src/error-handling/transactionErrorHandling.ts
+++ src/error-handling/transactionErrorHandling.ts
@@ -1,9 +1,18 @@
-
 import { IDL as JupiterIDL } from './jupiter';
 import { SendTransactionError, VersionedTransaction } from '@solana/web3.js';
 import * as idl from '../idl/wasabi_solana.json';
-import {WasabiSolana} from "../index";
+import { WasabiSolana } from "../index";
 const WasabiIDL = idl as WasabiSolana;
+
+export class SimulationError extends Error {
+    constructor(
+        public error: string,
+        public transaction: VersionedTransaction,
+        public logs?: string[]
+    ) {
+        super('SimulationError');
+    }
+}
 
 type ErrorObject = {
     code: number;
@@ -54,24 +63,57 @@ const findJupiterError = (code: number): ErrorObject | undefined => {
 }
 
 export const parseSendTransactionError = (error: SendTransactionError, transaction: VersionedTransaction): ErrorObject | undefined => {
-    const message = error.transactionError.message;
-
-    // Parse error messages like: "Transaction simulation failed: Error processing Instruction 3: custom program error: 0x1771"
+    const message = error.message;
     const match = message.match(/Instruction (\d+):.*(0x[0-9a-fA-F]+)/);
 
     if (match) {
         const instructionNumber = parseInt(match[1]); // First capture group
         const errorCode = parseInt(match[2], 16); // Second capture group, comes as hex
+        return parseError(instructionNumber, errorCode, transaction);
+    }
 
-        // Validate instruction against parsed addresses
-        const instruction = transaction.message.compiledInstructions[instructionNumber];
-        const programId = transaction.message.staticAccountKeys[instruction.programIdIndex].toBase58();
+    return matchComputeError(message);
+}
 
-        if (programId === wasabiProgramId) {
-            return findWasabiError(errorCode);
-        } else if (programId === jupiterProgramId) {
-            return findJupiterError(errorCode);
-        }
+export const parseError = (instructionNumber: number, errorCode: number, transaction: VersionedTransaction): ErrorObject | undefined => {
+    // Parse error messages like: "Transaction simulation failed: Error processing Instruction 3: custom program error: 0x1771"
+    // Validate instruction against parsed addresses
+    const instruction = transaction.message.compiledInstructions[instructionNumber];
+    const programId = transaction.message.staticAccountKeys[instruction.programIdIndex].toBase58();
+
+    if (programId === wasabiProgramId) {
+        return findWasabiError(errorCode);
+    } else if (programId === jupiterProgramId) {
+        return findJupiterError(errorCode);
     }
     return undefined;
-}
+};
+
+export const parseSimulationError = (error: SimulationError, transaction: VersionedTransaction): ErrorObject | undefined => {
+    const message = JSON.stringify(error.error);
+    const match = message.match(/\[(\d+),.*Custom.*?(\d+)/);
+
+    if (match) {
+        const instructionNumber = parseInt(match[1]);
+        const errorCode = parseInt(match[2]);
+        return parseError(instructionNumber, errorCode, transaction);
+    }
+
+    return matchComputeError(message);
+};
+
+const matchComputeError = (message: string): ErrorObject | undefined => {
+    const match = message.match(/.*"Program failed to complete"/);
+
+    if (match) {
+        return {
+            code: 0,
+            name: 'ProgramFailedToComplete',
+            msg: 'Program failed to complete',
+            expected: false,
+            program: 'ComputeBudget'
+        };
+    }
+
+    return undefined;
+};
